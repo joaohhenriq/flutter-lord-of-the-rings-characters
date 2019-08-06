6e81b7a7a4b8d9eb1386d170dd7bd487aaa6103a
joaoh <joao_camargo10@hotmail.com>
2019-08-06 20:49:43 -0300

    Characters and backgrounds

--- lib/pages/character_listing_page.dart
+++ lib/pages/character_listing_page.dart
@@ -23,38 +23,54 @@ class _CharacterListingPageState extends State<CharacterListingPage> {
           )
         ],
       ),
-      body: Column(
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.only(left: 32, top: 8),
-            child: RichText(
-              text: TextSpan(
-                children: [
-                  TextSpan(
-                      text: "Lord of the Rings",
-                      style: TextStyle(
-                        color: Colors.black87,
-                        fontSize: 30,
-                        fontWeight: FontWeight.w700,
-                      )),
-                  TextSpan(
-                    text: "\n",
-                  ),
-                  TextSpan(
-                    text: "Characters",
-                    style: TextStyle(
-                      color: Colors.black87,
-                      fontSize: 26,
-                    ),
-                  )
-                ],
+      body: SafeArea(
+        child: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              fit: BoxFit.cover,
+              image: AssetImage(
+                "assets/images/background1.jpg",
               ),
             ),
           ),
-          Expanded(
-            child: CharacterWidget(),
-          )
-        ],
+          child: Container(
+            color: Colors.black.withOpacity(0.4),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.only(left: 32, top: 8),
+                  child: RichText(
+                    text: TextSpan(
+                      children: [
+                        TextSpan(
+                            text: "Lord of the Rings",
+                            style: TextStyle(
+                              color: Colors.black87,
+                              fontSize: 30,
+                              fontWeight: FontWeight.w700,
+                            )),
+                        TextSpan(
+                          text: "\n",
+                        ),
+                        TextSpan(
+                          text: "Characters",
+                          style: TextStyle(
+                            color: Colors.black87,
+                            fontSize: 26,
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: CharacterWidget(),
+                )
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
--- lib/widgets/character_widget.dart
+++ lib/widgets/character_widget.dart
@@ -4,23 +4,107 @@ import 'package:lord_of_the_rings_characters/model/character_model.dart';
 class CharacterWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
     final screenHeight = MediaQuery.of(context).size.height;
+    final screenWidth = MediaQuery.of(context).size.width;
 
     return Stack(
       children: <Widget>[
         Container(
-          height: 0.55 * screenHeight,
-          width: 0.9 * screenHeight,
-          decoration: BoxDecoration(
-              gradient: LinearGradient(
-                colors: characters[0].colors,
-                begin: Alignment.topRight,
-                end: Alignment.bottomLeft,
-              )
+          padding: const EdgeInsets.only(bottom: 10),
+          child: Align(
+            alignment: Alignment.bottomCenter,
+            child: ClipPath(
+              clipper: CharacterCardBackgroundClipper(),
+              child: Container(
+                height: 0.6 * screenHeight,
+                width: 0.9 * screenWidth,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    fit: BoxFit.cover,
+                    image: AssetImage(
+                      characters[0].backgroundPath,
+                    ),
+                  ),
+                ),
+                child: Container(
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      colors: [
+                        Colors.black,
+                        Colors.black.withOpacity(0.8),
+                        Colors.black.withOpacity(0.5),
+                        Colors.black.withOpacity(0.7),
+                        Colors.black,
+                      ],
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                    ),
+                  ),
+                ),
+              ),
+            ),
           ),
         ),
+        Align(
+          alignment: Alignment(0.6, -0.45),
+          child: Image.asset(
+            characters[0].imagePath,
+            height: screenHeight * 0.60,
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(left: 48, right: 16, bottom: 30),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                characters[0].name,
+                style: TextStyle(
+                    fontWeight: FontWeight.w800,
+                    fontSize: 28,
+                    color: Colors.white),
+              ),
+              Text(
+                "Tap to read more",
+                style: TextStyle(
+                    fontWeight: FontWeight.w500,
+                    fontSize: 18,
+                    color: Colors.white),
+              )
+            ],
+          ),
+        )
       ],
     );
+  }
+}
+
+class CharacterCardBackgroundClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    Path clippedPath = Path();
+    double curveDistance = 40;
+
+    clippedPath.moveTo(0, size.height * 0.4);
+    clippedPath.lineTo(0, size.height - curveDistance);
+    clippedPath.quadraticBezierTo(
+        1, size.height - 1, 0 + curveDistance, size.height);
+    clippedPath.lineTo(size.width - curveDistance, size.height);
+    clippedPath.quadraticBezierTo(size.width + 1, size.height - 1, size.width,
+        size.height - curveDistance);
+    clippedPath.lineTo(size.width, 0 + curveDistance);
+    clippedPath.quadraticBezierTo(size.width - 1, 0,
+        size.width - curveDistance - 5, 0 + curveDistance / 3);
+    clippedPath.lineTo(curveDistance, size.height * 0.29);
+    clippedPath.quadraticBezierTo(
+        1, (size.height * 0.30) + 10, 0, size.height * 0.4);
+
+    return clippedPath;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) {
+    return true;
   }
 }
